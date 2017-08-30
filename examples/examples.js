dddb6f1c1dafdcc0cc88bfa4c475813d7b76b6ed
Laura Schlimmer <laura@zscale.io>
2017-08-30 17:02:50 +0200

    add search example

--- examples/examples.js
+++ examples/examples.js
@@ -1,17 +1,14 @@
+"use strict";
 
 class zDropdownExample {
-  constructor() {
-
-  }
-
-  init() {
+  constructor(elem) {
     var dropdown_items = [
       { title: "English", value: "en" },
       { title: "French", value: "fr" },
       { title: "Spanish", value: "es" }
     ]
 
-    var dropdown = viewport.querySelector("z-dropdown");
+    var dropdown = elem.querySelector("z-dropdown");
     dropdown.setMenuItems(dropdown_items);
     dropdown.addEventListener("change", (e) => {
       console.log(e.detail);
@@ -20,20 +17,54 @@ class zDropdownExample {
   }
 }
 
-class zExamples {
-  constructor(viewport) {
-    this.examples = [new zDropdownExample()];
+class zSearchExample {
+  constructor(elem) {
+    let countries = [
+      { value: "en", title: "England" },
+      { value: "fr", title: "France" },
+      { value: "be", title: "Belgium" },
+      { value: "nl", title: "Netherlands" },
+      { value: "pl", title: "Poland" },
+      { value: "ger", title: "Germany" },
+      { value: "es", title: "Spain" },
+      { value: "it", title: "Italy" },
+      { value: "grc", title: "Greece" },
+      { value: "prt", title: "Portugal" },
+      { value: "es", title: "Spain" },
+      { value: "uk", title: "United Kingdom" },
+      { value: "dk", title: "Denmark" },
+      { value: "swe", title: "Sweden" },
+      { value: "fin", title: "Finland" }
+    ];
 
-  }
+    let search = elem.querySelector("z-search");
+    search.init({autocomplete: {min_length: 1}});
+    search.setPlaceholder("Search countries...");
 
-  init() {
-    this.examples.forEach(function(example) {
-      example.init();
+    search.addEventListener("autocomplete", (e) => {
+      let value = e.detail.value.toLowerCase();
+      let c = countries.filter(function(country) {
+        return (country.title.toLowerCase().indexOf(value) >= 0)
+      });
+      search.autocomplete(e.detail.value, c);
     });
+
+    search.addEventListener("change", (e) => {
+      console.log("detail", e.detail);
+    });
+  }
+}
+
+class zExamples {
+  constructor(vport) {
+    this.examples = [
+        new zDropdownExample(vport),
+        new zSearchExample(vport)
+    ];
+
   }
 }
 
 document.addEventListener("DOMContentLoaded", () => {
   let examples = new zExamples(document.getElementById("viewport"));
-  examples.init();
 });
