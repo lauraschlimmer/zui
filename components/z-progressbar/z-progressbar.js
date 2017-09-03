8856d67bc0b256484037e0eedd2f22728fdcb930
Laura Schlimmer <laura@zscale.io>
2017-09-03 16:57:19 +0200

    refactor z-progressbar

--- components/z-progressbar/z-progressbar.js
+++ components/z-progressbar/z-progressbar.js
@@ -1,55 +1,29 @@
-var ProgressBarComponent = function() {
-  this.createdCallback = function() {
-    var tpl = zTemplateUtil.getTemplate("z-progressbar-base-tpl");
 
+class zProgressBar extends HTMLElement {
+  constructor() {
+    super();
+  }
+
+  render() {
+    let tpl = zTemplateUtil.getTemplate("z-progressbar-tpl");
     this.appendChild(tpl);
+    
 
-    if (this.hasAttribute('data-label')) {
-      this.renderDataLabel();
-    }
+  }
 
-     if (this.hasAttribute('data-progress')) {
-      this.updateProgressBar(this.getAttribute('data-progress'));
-    }
+  update(progress) {
+    let progress_value = (progress * 100).toFixed() + "%"
+    let bar_elem = this.querySelector(".bar");
+    bar_elem.style.width = progress_value;
 
-    this.attributeChangedCallback();
-  };
+    var progress_elem = this.querySelector(".progress");
+    progress_elem.innerHTML = progress_value;
+  }
 
-  this.attributeChangedCallback = function(attr, old_val, new_val) {
-    switch (attr) {
-      case "data-progress":
-        this.updateProgressBar(new_val);
-        break;
-      case "data-label":
-        this.renderDataLabel();
-        break;
-      default:
-        break;
-    }
-  };
+  setLabel(text) {
+    let label_elem = this.querySelector(".label");
+    label_elem.innerHTML = zDomUtil.escapeHTML(text);
+  }
+}
 
-  this.renderDataLabel = function() {
-    var label_elem = this.querySelector(".label");
-    if (!label_elem) {
-      label_elem = document.createElement("div");
-      label_elem.className = "label";
-      this.appendChild(label_elem);
-    }
-
-    label_elem.innerHTML = this.getAttribute('data-label');
-  };
-
-  this.updateProgressBar = function(progress) {
-    var bar_elem = this.querySelector(".bar");
-    bar_elem.style.width = progress + "%";
-
-    if (!(this.getAttribute('data-show-progress') == "off")) {
-      var progress_elem = this.querySelector(".progress");
-      progress_elem.innerHTML = parseFloat(progress).toFixed(1) + "%";
-    }
-  };
-};
-
-var proto = Object.create(HTMLElement.prototype);
-ProgressBarComponent.apply(proto);
-document.registerElement("z-progressbar", { prototype: proto });
+customElements.define("z-progressbar", zProgressBar);
