90ca633c634c8485e57231be11b340e299d86395
Laura Schlimmer <laura@zscale.io>
2017-09-03 15:54:20 +0200

    refactor z-modal

--- components/z-modal/z-modal.js
+++ components/z-modal/z-modal.js
@@ -1,64 +1,36 @@
-/**
-  This file is part of the "FnordMetric" project
-    Copyright (c) 2014 Laura Schlimmer
-    Copyright (c) 2014 Paul Asmuth, Google Inc.
 
-  FnordMetric is free software: you can redistribute it and/or modify it under
-  the terms of the GNU General Public License v3.0. You should have received a
-  copy of the GNU General Public License along with this program. If not, see
-  <http://www.gnu.org/licenses/>.
-**/
-var ModalComponent = function() {
-  this.createdCallback = function() {
-    var _this = this;
-    this.onclick = function(e) {
-      _this.close();
-    };
+class zModal extends HTMLElement {
+  constructor() {
+    super();
 
-    this.querySelector("z-modal-close-icon").onclick = function(e) {
-      _this.close();
-    };
+    window.setTimeout(() => {
+      this.addEventListener("click", this.hide, false);
 
-    this.querySelector("z-modal-box").onclick = function(e) {
-      var target = e.srcElement || e.target;
-      if (target.tagName == "Z-MODAL-CLOSE-ICON") {
-        _this.close();
-        return;
-      }
-      e.stopPropagation();
-    };
-  };
-
-  this.show = function() {
-    this.setAttribute('data-active', 'active');
-
-    var on_escape = function(e) {
-      if (e.keyCode == 27) {
-        this.close();
-      }
-    };
-
-    //place modal box
-    var modal_box = this.querySelector("z-modal-box");
-    var height = modal_box.getBoundingClientRect().height;
-    modal_box.style.top = ((window.innerHeight - height) / 4) + "px";
-
-    this.escape_listener = on_escape.bind(this);
-    document.addEventListener('keyup', this.escape_listener, false);
-  };
-
-  this.close = function() {
-    this.setAttribute('data-active', "");
-    document.removeEventListener('keyup', this.escape_listener, false);
-    var ev = new CustomEvent("z-modal-close", {
-      bubbles: true,
-      cancelable: true
+      var window_elem = this.querySelector("z-modal-window");
+      window_elem.addEventListener("click", (e) => {
+        e.stopPropagation();
+      }, false);
     });
 
-    this.dispatchEvent(ev);
-  };
-};
+  }
 
-var proto = Object.create(HTMLElement.prototype);
-ModalComponent.apply(proto);
-document.registerElement("z-modal", { prototype: proto });
+  show() {
+    var on_escape = function(e) {
+      if (e.keyCode == 27) {
+        this.hide();
+      }
+    };
+
+    this.escape_listener = on_escape.bind(this);
+
+    this.classList.add("active");
+    document.addEventListener("keyup", this.escape_listener, false);
+  }
+
+  hide() {
+    this.classList.remove("active");
+    document.removeEventListener("keyup", this.escape_listener, false);
+  }
+}
+
+customElements.define("z-modal", zModal);
